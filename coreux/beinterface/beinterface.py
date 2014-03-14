1c1f0422679295dbe1fd5dc20211eeb8b5563f8e
Rui Ferreira <ruiandreferreira@gmail.com>
2014-03-14 05:26:22 +0000

    Feed parser

--- coreux/beinterface/beinterface.py
+++ coreux/beinterface/beinterface.py
@@ -10,7 +10,7 @@ class BackendInterface:
         try:
             sock.connect(("localhost", 8001))
             sock.sendall(pickle.dumps(request))
-            response = pickle.loads(sock.recv(1024))
+            response = pickle.loads(sock.recv(20 * 1024))
             response["result"] = "success"
             logging.info("Request: " + str(request) + " Response: " + str(response))
             sock.close()
